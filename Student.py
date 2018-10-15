b6eb0c33ccbd97d45b2c6a2b173f539f79ea5bfd
TaoEnd <769524295@qq.com>
2018-10-15 19:01:51 +0800

    创建Student类

---
+++ Student.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+
+class Student:
+	# 属性
+	name = "NoName"
+	age = 0
+	tel = "00000000"
+	score = 0
+
+	# 动作
+	def listen(self):
+		pass
+
+	def test(self, name, time, addr):
+		pass
