9d4c75381a3ffb53eb484117d852cfcae2f3a900
TaoEnd <769524295@qq.com>
2018-10-15 19:12:03 +0800

    完成了Student类

--- Student.py
+++ Student.py
@@ -9,8 +9,21 @@ class Student:
 	score = 0
 
 	# 动作
+	# self表示把对象自身传入函数
 	def listen(self):
-		pass
+		print(self.name, self.score)
 
 	def test(self, name, time, addr):
 		pass
+
+
+# 定义一个叫张三的学生
+zhangsan = Student()
+zhangsan.name = "zhangsan"
+zhangsan.age = 18
+zhangsan.tel = "12345678"
+zhangsan.score = 85.5
+zhangsan.listen()
+zhangsan.test("python", "today", "classroom1201")
+
+print(zhangsan.name, zhangsan.age)
