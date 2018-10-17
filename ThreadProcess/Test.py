42c6f7728d3b992a359d078902c90936fe24f464
TaoEnd <769524295@qq.com>
2018-10-17 11:27:06 +0800

    进程间通信

--- ThreadProcess/Test.py
+++ ThreadProcess/Test.py
@@ -3,30 +3,25 @@
 from multiprocessing import Pool
 import os
 
-target = r"E:\python\PythonSpace\Git\Python\ThreadProcess\test\target"
 
-def copyFile(li):
+def copyFile(sourcePath, targetPath):
 	print("子进程%d开始" % os.getpid())
-	for path in li:
-		name = path.split('\\')[-1]
-		resultPath = target + '\\' + name
-		fr = open(path, 'rb')
-		fw = open(resultPath, 'wb')
-		context = fr.read()
-		fw.write(context)
+	fr = open(sourcePath, 'rb')
+	fw = open(targetPath, 'wb')
+	context = fr.read()
+	fw.write(context)
 	print("子进程%d结束" % os.getpid())
 
 if __name__ == "__main__":
 	source = r"E:\python\PythonSpace\Git\Python\ThreadProcess\test\source"
+	target = r"E:\python\PythonSpace\Git\Python\ThreadProcess\test\target"
 	fileList = os.listdir(source)
-	nameList = []
-	for file in fileList:
-		nameList.append(os.path.join('%s\%s'%(source,file)))
 	print("父进程启动")
 	p = Pool(3)
-	p.apply_async(copyFile, args=(nameList[:3],))
-	p.apply_async(copyFile, args=(nameList[3:6],))
-	p.apply_async(copyFile, args=(nameList[6:],))
+	for file in fileList:
+		sourcePath = os.path.join(source, file)
+		targetPath = os.path.join(target, file)
+		p.apply_async(copyFile, args=(sourcePath, targetPath))
 	p.close()
 	p.join()
 	print("父进程结束")
